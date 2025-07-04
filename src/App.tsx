dc7e174caec6d037c4ef0f2e7b0eac8a6ee840f1
Derikyan <armen.derikyan@gmail.com>
2025-07-04 08:42:33 +0400

    change routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import FormWrapper from "./FormWrapper";
 import Step1 from "./pages/Step1";
 import Step2 from "./pages/Step2";
@@ -9,12 +9,10 @@ function App() {
     <BrowserRouter>
       <FormWrapper>
         <Routes>
-          <Route path="/" element={<Outlet />}>
-            <Route index element={<Step1 />} />
-            <Route path="loan-app" element={<Step1 />} />
-            <Route path="step2" element={<Step2 />} />
-            <Route path="step3" element={<Step3 />} />
-          </Route>
+          <Route path="/" element={<Navigate to="/loan-app" replace />} />
+          <Route path="/loan-app" element={<Step1 />} />
+          <Route path="/step2" element={<Step2 />} />
+          <Route path="/step3" element={<Step3 />} />
         </Routes>
       </FormWrapper>
     </BrowserRouter>
